d7633f6d985f62e0d8f2584c6d8f8beb6fa5666e
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-15 16:22:25 -0600

    Initial project commit

---
+++ DiceApp/DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by Joshua Rechkemmer on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
